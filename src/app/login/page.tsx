506edea5604a8c0dd7a5a0c891c57d89cb8089a0
Thomas Scott <t.scott12331@gmail.com>
2025-03-08 15:21:56 -0800

    styled login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,34 @@
 "use client"
 
+import styles from './page.module.css';
+
 import { login } from "@/lib/auth"
 import { useActionState } from "react"
+
+import FormInput from '@/components/inputs/form-input';
+import FormButton from '@/components/inputs/form-button';
+import Link from 'next/link';
 
 export default function Page() {
     const [data, action, isPending] = useActionState(login, undefined);    
 
     return (
-        <form action={action}>
-            <input name="username" />
-            <input name="password" type="password" />
-            <button type="submit"></button>
-            <p>{data?.error}</p>
-        </form>
+        <div className={styles.page}>
+            <form action={action}
+            className={styles.form}
+            >
+                <div className={styles.formTitleWrapper}>
+                    <h2>Log In</h2>
+                </div>
+                <div className={styles.formInputs}>
+                    <FormInput name="username" labelText="Username" />
+                    <FormInput name="password" labelText="Password" type="password" />
+                    <FormButton text="Create" />
+                </div>
+                <div className={styles.formFoot}>
+                    <Link href="/register">Don't have an account?</Link>
+                </div>
+            </form>
+        </div>
     )
 }
